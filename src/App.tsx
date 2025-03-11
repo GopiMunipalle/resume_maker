605484280a446b550bd45282f5cf5bad27883a41
GopiMunipalli <munipallegopikrishna@gmail.com>
2025-03-11 22:31:58 +0530

    app.tsx updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Templates from "./pages/Templates";
@@ -11,49 +11,44 @@ import NotFound from "./components/NotFound";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route
-          path="/home"
-          element={<ProtectedRoutes allowedRoles={["user"]} Component={Home} />}
-        />
-        <Route
-          path="/templates"
-          element={
-            <ProtectedRoutes allowedRoles={["user"]} Component={Templates} />
-          }
-        />
-        <Route
-          path="/profile"
-          element={
-            <ProtectedRoutes allowedRoles={["user"]} Component={Profile} />
-          }
-        />
-        <Route
-          path="/create-resume"
-          element={
-            <ProtectedRoutes
-              allowedRoles={["user"]}
-              Component={TemplateEditor}
-            />
-          }
-        />
-        <Route
-          path="/resume/:id"
-          element={
-            <ProtectedRoutes allowedRoles={["user"]} Component={ResumeItem} />
-          }
-        />
-        <Route
-          path="/help"
-          element={
-            <ProtectedRoutes allowedRoles={["user"]} Component={HelpPage} />
-          }
-        />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route
+        path="/home"
+        element={<ProtectedRoutes allowedRoles={["user"]} Component={Home} />}
+      />
+      <Route
+        path="/templates"
+        element={
+          <ProtectedRoutes allowedRoles={["user"]} Component={Templates} />
+        }
+      />
+      <Route
+        path="/profile"
+        element={
+          <ProtectedRoutes allowedRoles={["user"]} Component={Profile} />
+        }
+      />
+      <Route
+        path="/create-resume"
+        element={
+          <ProtectedRoutes allowedRoles={["user"]} Component={TemplateEditor} />
+        }
+      />
+      <Route
+        path="/resume/:id"
+        element={
+          <ProtectedRoutes allowedRoles={["user"]} Component={ResumeItem} />
+        }
+      />
+      <Route
+        path="/help"
+        element={
+          <ProtectedRoutes allowedRoles={["user"]} Component={HelpPage} />
+        }
+      />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
 
